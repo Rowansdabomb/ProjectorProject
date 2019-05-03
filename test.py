e18d0da98ccc83d5ba4768360c09200bcb31e66c
rowansdabomb <rowanskye@gmail.com>
2019-05-03 11:05:22 -0700

    block out faces from projector, convert mask from white to black

--- test.py
+++ test.py
@@ -1,24 +1,35 @@
 # import the necessary packages
-from collections import deque
 from imutils.video import VideoStream
+from imutils.video import FPS
 import numpy as np
 import argparse
-import cv2
 import imutils
 import time
+import cv2
  
+####################### HANDLE ARGUMENTS ##########################
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-colorLower = (29, 86, 6)
-colorUpper = (64, 255, 255)
-screenWidth = 900
+###################################################################
+
+################### OBJECT DETECTION SETUP ########################
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+####################################################################
+
+########################### VIDEO SETUP ############################
 
 # if a video path was not supplied, grab the reference
 # to the webcam
@@ -31,6 +42,11 @@ else:
  
 # allow the camera or video file to warm up
 time.sleep(.25)
+fps = FPS().start()
+
+####################################################################
+
+########################## GLOBAL VARIABLES ########################
 
 initialized = False
 mean = None
@@ -38,19 +54,29 @@ mean = None
 font = cv2.FONT_HERSHEY_PLAIN
 lineType = cv2.LINE_AA
 
-pts = deque(maxlen=100)
-
+frame = None
 overlay = None
+personBox = None
+faceMask = None
 
-def initializeColor(frame):
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize the
+# list of tracked points
+colorLower = (29, 86, 6)
+colorUpper = (64, 255, 255)
+screenWidth = 900
+
+####################################################################
+
+def initializeColor():
   global mean
+  global frame
   # left, top, right, bottom
   size = 75
   c1 = int((frame.shape[1] / 2 - size/2))
   r1 = int((frame.shape[0] / 2 - size/2))
 
   roi = frame[r1:c1, r1+size:c1+size]
-  cv2.imshow("roi", roi)
 
   cv2.rectangle(frame, (c1, r1), (c1+size, r1 + size), (255, 255, 255), 1)
   mean = cv2.mean(roi)
@@ -94,6 +120,41 @@ def captureColor():
   colorUpper = np.array([maxHue, maxSat, maxVal])
   time.sleep(1.0)
 
+def detectFaces():
+  global frame
+  global net
+  global faceMask
+  # grab the frame dimensions and convert it to a blob
+  (h, w) = frame.shape[:2]
+  blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+    (300, 300), (104.0, 177.0, 123.0))
+
+  # pass the blob through the network and obtain the detections and
+  # predictions
+  net.setInput(blob)
+  detections = net.forward()
+
+  # loop over the detections
+  for i in range(0, detections.shape[2]):
+    # extract the confidence (i.e., probability) associated with the
+    # prediction
+    confidence = detections[0, 0, i, 2]
+
+    # filter out weak detections by ensuring the `confidence` is
+    # greater than the minimum confidence
+    if confidence < args["confidence"]:
+      continue
+
+    # compute the (x, y)-coordinates of the bounding box for the
+    # object
+    face = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+    (startX, startY, endX, endY) = face.astype("int")
+    center = (int(startX + (endX - startX)/2), int(startY + (endY - startY)/2))
+    
+    faceMask = np.ones((frame.shape[0], frame.shape[1]), np.uint8)
+    # draw a circle around the persons head
+    cv2.circle(faceMask, center, int((endX - startX) / 2), 255, -1)
+
 # keep looping
 while True:
   # grab the current frame
@@ -111,8 +172,11 @@ while True:
   # resize the frame
   frame = imutils.resize(frame, width=screenWidth)
 
+  detectFaces()
+
   if not initialized:
-    initializeColor(frame)
+    initializeColor()
+
   # blur the frame, and convert it to the HSV
   # color space
   blurred = cv2.GaussianBlur(frame, (11, 11), 0)
@@ -132,7 +196,6 @@ while True:
 
   center = None
   radius = 0
-  currentMask = None
 
   # only proceed if at least one contour was found
   if len(cnts) > 0:
@@ -148,11 +211,8 @@ while True:
     if radius > screenWidth / 100:
       # draw the circle and centroid on the frame and overlayMask,
       # then update the list of tracked points
-      currentMask = np.zeros((frame.shape[0], frame.shape[1]), np.uint8)
+      currentMask = np.ones((frame.shape[0], frame.shape[1]), np.uint8)
       cv2.circle(currentMask, (int(x), int(y)), int(radius), 255, -1)
-      
-      # cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 1)
-      # cv2.circle(frame, center, 3, (0, 0, 255), -1)
   
   if initialized:
     if overlay is None:
@@ -160,11 +220,13 @@ while True:
     elif currentMask is not None:
       # create a historical mask
       overlay = cv2.bitwise_or(overlay, currentMask)
-      cv2.imshow("mask", overlay)
 
     # # c2.imshow("mask", overlay)
-    final = cv2.cvtColor(cv2.bitwise_not(overlay), cv2.COLOR_GRAY2BGR)
-    frame = cv2.addWeighted(final, 1, frame, 1, 0)
+    temp = cv2.bitwise_or(cv2.bitwise_not(overlay), faceMask)
+    final = cv2.bitwise_not(cv2.cvtColor(temp, cv2.COLOR_GRAY2BGR))
+    cv2.imshow("overlay", final)
+    frame = cv2.addWeighted(final, 1, frame, 1, -255)
+    
 
   cv2.putText(frame, str(int(radius)), (10, 30), font, 2, (0, 0, 255), 2, lineType)
   # show the frame to our screen
@@ -181,8 +243,15 @@ while True:
   if key == ord("s"):
     captureColor()
     initialized = True
+
+  fps.update()
     
  
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
 # if we are not using a video file, stop the camera video stream
 if not args.get("video", False):
 	vs.stop()
